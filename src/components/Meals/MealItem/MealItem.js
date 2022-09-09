5c680545cecc29119c262fd5ea8972bba9bb837e
elron <elronkarni123@gmail.com>
2022-09-09 22:01:35 +0300

    first commit

---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+import MealItemForm from "./MealItemForm";
+import classes from "./MealItem.module.css";
+
+const MealItem = (props) => {
+  const mealPrice = `$${props.mealPrice.toFixed(2)}`;
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.mealName}</h3>
+        <div className={classes.description}>{props.mealDescription}</div>
+        <div className={classes.price}>{mealPrice}</div>
+      </div>
+      <div>
+        <MealItemForm id={props.id} />
+      </div>
+    </li>
+  );
+};
+
+export default MealItem;
