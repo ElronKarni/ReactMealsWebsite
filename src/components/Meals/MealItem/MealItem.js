5c0b45c4db00f653406342803a64fdf68eb26138
elron <elronkarni123@gmail.com>
2022-09-10 23:44:11 +0300

    finished Section 11

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import MealItemForm from "./MealItemForm";
+import CartContext from "../../../store/cart-context";
 import classes from "./MealItem.module.css";
 
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
   const mealPrice = `$${props.mealPrice.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.mealName,
+      amount: amount,
+      price: props.mealPrice,
+    });
+  };
   return (
     <li className={classes.meal}>
       <div>
@@ -12,7 +23,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{mealPrice}</div>
       </div>
       <div>
-        <MealItemForm id={props.id} />
+        <MealItemForm onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
